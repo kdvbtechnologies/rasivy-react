d59f58318210f2aac5ba4d84b730896bc23cf396
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-07-17 08:28:31 +0200

    m

---
+++ src/context/InstallContext.js
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect, useState } from "react";
+
+export const InstallContext = React.createContext(null);
+
+export const InstallProvider = ({ children }) => {
+  const [deferredPrompt, setDeferredPrompt] = useState();
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
+      e.preventDefault();
+
+      setDeferredPrompt(e);
+    };
+
+    const handleAppInstalled = () => {
+      setDeferredPrompt();
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+    window.addEventListener("appinstalled", handleAppInstalled);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+      window.removeEventListener("appinstalled", handleAppInstalled);
+    };
+  }, []);
+
+  return (
+    <InstallContext.Provider value={deferredPrompt}>
+      {children}
+    </InstallContext.Provider>
+  );
+};
+
+export const useInstall = () => useContext(InstallContext);
