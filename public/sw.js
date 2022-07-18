7da126908d6d536445b1a3f2879614d7f8f3f40f
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-07-18 11:58:26 +0200

    pwa offline mode

---
+++ public/sw.js
@@ -0,0 +1,71 @@
+const BASE = location.protocol + "//" + location.host;
+const PREFIX = "V1";
+const CACHED_FILES = [
+  /*"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+   */
+  `${BASE}/App.js`,
+  `${BASE}/sw.js`,
+  `${BASE}/static/js/bundle.js`,
+  `${BASE}/manifest.json`,
+  `${BASE}/favicon.ico`,
+  `${BASE}/logo192.png`,
+  `${BASE}/logo.svg`,
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(PREFIX);
+      await Promise.all(
+        [...CACHED_FILES, "/index.html"].map((path) => {
+          return cache.add(new Request(path));
+        })
+      );
+    })()
+  );
+  console.log(`${PREFIX} Install`);
+});
+
+self.addEventListener("activate", (event) => {
+  clients.claim();
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(
+        keys.map((key) => {
+          if (!key.includes(PREFIX)) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })()
+  );
+  console.log(`${PREFIX} Active`);
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log(
+    `${PREFIX} Fetching : ${event.request.url}, Mode : ${event.request.mode}`
+  );
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+          return await fetch(event.request);
+        } catch (error) {
+          // if offline mode
+          const cache = await caches.open(PREFIX);
+          return await cache.match("/index.html");
+        }
+      })()
+    );
+  } else if (CACHED_FILES.includes(event.request.url)) {
+    event.respondWith(caches.match(event.request));
+  }
+});
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./i18n";
-import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,5 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-serviceWorker.register();
