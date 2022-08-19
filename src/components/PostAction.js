c5f9c6ae05ad33498556c4ecbf7b2f0fb6266ada
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-08-19 05:12:10 +0000

    m

--- src/components/PostAction.js
+++ src/components/PostAction.js
@@ -49,6 +49,79 @@ export default function PostAction() {
       <div id="post" className={`${dark ? "dark" : "light"}`}>
         <h1>{t("post")}</h1>
         <div>
+          <br />
+          <br />
+          <div>
+            <div>
+              <input
+                type="text"
+                name="post"
+                value={post}
+                onChange={(e) => setPost(e.target.value)}
+                placeholder="Write your post here"
+                required
+              />
+            </div>
+            <button type="submit">Post</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+/*
+import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
+import { useState } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+
+export default function PostAction() {
+  const { t } = useTranslation();
+  const [dark] = useState(localStorage.getItem("dark-mode") === "true");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [post, setPost] = useState("");
+  const cookies = new Cookies();
+
+  function Signin(e) {
+    e.preventDefault();
+    axios({
+      method: "post",
+      //url: `https://api-adoony.herokuapp.com/api/v1/auth/signin`,
+      url: `http://localhost:5000/api/v1/auth/signin`,
+      data: {
+        email,
+        password,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+      //credentials: "include",
+    })
+      .then((res) => {
+        const name = res.data.blobName;
+        const value = res.data.blobValue;
+        cookies.set(`${name}`, `${value}`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  /* input de type="file"  
+
+  return (
+    <>
+      <Helmet>
+        <body className={`${dark ? "dark-body" : "light-body"}`}></body>
+      </Helmet>
+
+      <div id="post" className={`${dark ? "dark" : "light"}`}>
+        <h1>{t("post")}</h1>
+        <div>
           <h1>Connexion</h1>
           <form action="" onSubmit={Signin}>
             <div>
@@ -93,3 +166,4 @@ export default function PostAction() {
     </>
   );
 }
+*/
