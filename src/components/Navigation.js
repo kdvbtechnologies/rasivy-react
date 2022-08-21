ec61a4a80e861d06d2df8f4d6eb0ebd8b6e21193
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-08-21 06:48:36 +0200

    m

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Helmet } from "react-helmet";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Navigation() {
   const { t } = useTranslation();
   const [dark] = useState(localStorage.getItem("dark-mode") === "true");
+
+  useEffect(() => {}, [dark]);
 
   return (
     <>
