51ad163f0d0c1161303e18639f31415752211db7
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-08-30 00:41:00 +0200

    m

--- src/components/LogoutAction.js
+++ src/components/LogoutAction.js
@@ -14,7 +14,7 @@ export default function LogoutAction() {
       </Helmet>
 
       <div id="settings" className={`${dark ? "dark" : "light"}`}>
-        <div className="back-btn">
+        <div className="back-btn-logout">
           <button onClick={() => navigate(-1)}>{t("--return")}</button>
         </div>
         <h1>{t("--logout")}</h1>
