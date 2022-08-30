c4462ade5e8bd6b7b7d36ebe4f4a5724fc789af1
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-08-30 17:41:43 +0200

    m

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./i18n";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+//dev tools
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
---
+++ src/reducers/user.reducer.js
@@ -0,0 +1,8 @@
+//Tout nos reducers auront un initial State (c-a-d un State de base)
+const initialState = {};
+
+/* Ici on Cree une function userReducer en mode export default qui aura un state(le state aura en
+parametre un initialState qui est vide au debut) et une action */
+export default function userReducer(state = initialState, action) {
+  //
+}
