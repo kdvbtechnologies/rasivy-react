a070e212c7ed919a8057f712401766cd806791d3
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-05 04:09:28 +0200

    m

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,7 +27,7 @@ export default function Login() {
     }).then((res) => {
       console.log(res);
       dispatch(setLogin(res));
-      if (res) {
+      if (!res) {
         const tokenStore = res.data.token;
         localStorage.setItem("https://jamelfase.com/user-token", tokenStore);
         setAfterLogin(`${t("--signin-success")}`);
