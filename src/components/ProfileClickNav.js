02120da04bd52a5d54293b6428663552f74e9926
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-06 18:11:48 +0200

    m

--- src/components/ProfileClickNav.js
+++ src/components/ProfileClickNav.js
@@ -1,7 +1,24 @@
+import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function ProfileClickNav() {
+  const { t } = useTranslation();
+  const [dark] = useState(localStorage.getItem("dark-mode") === "true");
+  const navigate = useNavigate();
+
   return (
     <>
-      <h1>hello, here is ProfileClickNav</h1>
+      <Helmet>
+        <body className={`${dark ? "dark-body" : "light-body"}`}></body>
+      </Helmet>
+      <div id="menu-navigation" className={`${dark ? "dark" : "light"}`}>
+        <div className="back-btn">
+          <button onClick={() => navigate(-1)}>{t("--return")}</button>
+          <h1>Profil de Zacharie</h1>
+        </div>
+      </div>
     </>
   );
 }
