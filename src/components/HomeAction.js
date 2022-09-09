2857c855372f413a091ee9043dd2175ab601de14
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-09 10:47:40 +0200

    m

--- src/components/HomeAction.js
+++ src/components/HomeAction.js
@@ -63,9 +63,6 @@ export default function HomeAction() {
         {posts?.map((post) => (
           <div className="posts" key={post.id}>
             <NavLink className="navlink-profile-click" to="/ProfileClick">
-              <div className="p">
-                <p>i</p>
-              </div>
               <h5>{post.username}</h5>
             </NavLink>
             <div className="post">{post.desc}</div>
