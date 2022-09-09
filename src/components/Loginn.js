ded0ed2ff7cc829dc06c3352029bf72a6d3be2e8
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-09 19:12:06 +0200

    m

--- src/components/Loginn.js
+++ src/components/Loginn.js
@@ -2,6 +2,7 @@ export default function Loginn() {
   return (
     <div>
       <h1>Login</h1>
+	  <p>Let's go, login now !</p>
     </div>
   );
 }
--- src/components/Signupp.js
+++ src/components/Signupp.js
@@ -2,6 +2,7 @@ export default function Signupp() {
   return (
     <div>
       <h1>Signup</h1>
+	  <p>Let's go, signup now !</p>
     </div>
   );
 }
