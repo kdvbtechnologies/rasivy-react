e9b5c7884e6c5e51cef2b5e2339b3180dfe600ef
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-09-12 14:21:33 +0200

    m

--- src/components/HomeAction.js
+++ src/components/HomeAction.js
@@ -19,6 +19,7 @@ export default function HomeAction() {
   const postUsername = useSelector((state) => state.posts.posts.username);
   //const [postss, setPostss] = useState("");
 
+  console.log(posts);
   console.log(postUsername);
   //getAllPost
   async function MyPosts() {
