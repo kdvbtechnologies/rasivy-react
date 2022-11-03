29f419ecdddff17baff83b8de74e607986addf31
kilendo-dingha-github <kilendodingha@rinabel.com>
2022-11-03 10:21:16 -0200

    m

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,16 +1,21 @@
-import { useEffect } from "react";
+//import { useEffect } from "react";
 
 export default function Test() {
-  function installButton() {
-    console.log("download");
-    let prompt = "hello";
-    prompt.prompt();
+  window.addEventListener("beforeinstallprompt", (e) => {
+    const deferredPrompt = e;
+    console.log(deferredPrompt);
+  });
+
+  /*
+  function installButton(e) {
+    console.log("fenetre download");
+    deferredPrompt = e;
   }
 
   useEffect(() => {
-    window.addEventListener("click", installButton);
+    window.addEventListener("beforeinstallprompt", installButton);
   }, []);
-
+*/
   return (
     <>
       <button>Install</button>
